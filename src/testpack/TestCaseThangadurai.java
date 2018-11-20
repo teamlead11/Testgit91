c649c12112a3df6b49ae844485887087f516d92e
ThangaduraiJayakumar <jaidurai773@gmail.com>
2018-11-21 02:03:14 +0530

    Test case added by Thangadurai

---
+++ src/testpack/TestCaseThangadurai.java
@@ -0,0 +1,12 @@
+package testpack;
+
+public class TestCaseThangadurai {
+
+	private void projectclone() {
+System.out.println("cloned");
+	}
+	public static void main(String[] args) {
+		TestCaseThangadurai tc = new TestCaseThangadurai();
+		tc.projectclone();
+	}
+}
