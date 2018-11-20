8e15306cc088847679af4a33c371e2cb4e145fdc
lsathish98 <45178403+lsathish98@users.noreply.github.com>
2018-11-20 23:56:49 +0530

    Add files via upload

---
+++ Userlsathish98GIT/src/org/user/lsathish98/git/GitUserlsathish98.java
@@ -0,0 +1,13 @@
+package org.user.lsathish98.git;
+
+public class GitUserlsathish98 {
+	public void methodOne() {
+		System.out.println("User lsathish98 Git Practice");
+
+	}
+
+	public static void main(String[] args) {
+		GitUserlsathish98 c=new GitUserlsathish98();
+		c.methodOne();
+	}
+}
