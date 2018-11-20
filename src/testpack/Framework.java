e14eed6d6d85968b3ea934aa5d57281753cd3d9b
teamlead11 <automationlead11@gmail.com>
2018-11-20 20:30:09 +0530

    added framework methods

---
+++ src/testpack/Framework.java
@@ -0,0 +1,13 @@
+package testpack;
+
+public class Framework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		frameworkMethods();
+	}
+
+	public static void frameworkMethods() {
+		System.out.println("framework created by lead");
+	}
+}
