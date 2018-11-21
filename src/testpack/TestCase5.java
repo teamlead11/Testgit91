3c645db27965f36e0131696860e66a494c0c5466
unknown <Gopinathanraman13790>
2018-11-22 00:16:44 +0530

    Test case5 change commited

--- src/testpack/TestCase5.java
+++ src/testpack/TestCase5.java
@@ -5,11 +5,18 @@ public class TestCase5 {
 	private void Employee() {
 		
       System.out.println("Employee information Details");
-      	}
+      
+	}
+	private void Employee1() {
+		
+	      System.out.println("Employee information Details");
+	      
+		}
 	
 	public static void main(String[] args) {
 		TestCase5 t=new TestCase5();
 		t.Employee();
+		t.Employee1();
 		
 	}
 
