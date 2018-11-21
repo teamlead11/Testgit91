49e54d3f5026d28433f676a5fa4fea7ffe3e286b
dkdeepakkumar3345 <dkdeepakkumar766@gmail.com>
2018-11-21 15:31:30 +0530

    Deepak added new testcase

---
+++ src/testpack/TestCaseDeepak.java
@@ -0,0 +1,14 @@
+package testpack;
+
+public class TestCaseDeepak {
+	public void sampleInfo() {
+
+		System.out.println("Deepak Test Case");
+
+	}
+
+	public static void main(String[] args) {
+		TestCaseDeepak deep = new TestCaseDeepak();
+		deep.sampleInfo();
+	}
+}
