981afb3480f7b3797934841d5cf91acf9c843e70
unknown <Gopinathanraman13790>
2018-11-21 23:44:56 +0530

    Test Case5 Commited

---
+++ src/testpack/TestCase5.java
@@ -0,0 +1,15 @@
+package testpack;
+
+public class TestCase5 {
+	
+	private void Employee() {
+		// TODO Auto-generated method stub
+      System.out.println("Employee information Details");
+	}
+	public static void main(String[] args) {
+		TestCase5 t=new TestCase5();
+		t.Employee();
+		
+	}
+
+}
