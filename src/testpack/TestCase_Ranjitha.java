74301d7ab8513499f88c54e25c20fdc0fa1a2248
RanjithaKolangi <ranjinathan10@gmail.com>
2018-11-21 08:11:33 +0530

    added testcase

---
+++ src/testpack/TestCase_Ranjitha.java
@@ -0,0 +1,15 @@
+package testpack;
+
+public class TestCase_Ranjitha {
+	
+
+		private void projectclone() {
+	System.out.println("cloned");
+		}
+		public static void main(String[] args) {
+			TestCase_Ranjitha tc = new TestCase_Ranjitha();
+			tc.projectclone();
+		}
+	}
+
+
