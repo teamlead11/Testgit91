db0078b365a1f77410fa6cc406a59d79dd53f2e5
teamlead11 <automationlead11@gmail.com>
2018-11-21 20:49:57 +0530

    completed test case3

---
+++ src/testpack/TestCase3.java
@@ -0,0 +1,13 @@
+package testpack;
+
+public class TestCase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testCase3();
+	}
+
+	public static void testCase3() {
+		System.out.println("test case 3");
+	}
+}
