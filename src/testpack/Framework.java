356e1a8088534bb485794c8c3d435e6e9ac035b5
manivelsele <rajamanivel@gmail.com>
2018-11-22 10:59:17 +0530

    Palindrom added in manivel method

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -1,5 +1,7 @@
 package testpack;
 
+import java.util.Scanner;
+
 public class Framework {
 
 	public static void main(String[] args) {
@@ -23,5 +25,22 @@ public class Framework {
 	public static void frameworkMethodsByuser1() {
 		System.out.println("modifed framework method by user1");
 	}
-
+	
+	public void paLindromeByManivel() {
+		String rev="";
+		System.out.println("Enter the string");
+		Scanner aal = new Scanner(System.in);
+		String name = aal.next();
+		int j=name.length();
+		System.out.println(j);
+		for (int i = j-1; i >=0; i--) {
+			rev=rev+name.charAt(i);
+		}
+		if (name.equals(rev)) {
+			System.out.println("String is palindrome");
+		}
+		else {
+			System.out.println("String is not palindrome");
+		}
+	}
 }
