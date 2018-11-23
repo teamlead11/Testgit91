8ce9061183b6d02b8c589803a20eee6640a8f916
005951 <005951@PCHNWC-198.spi-global.com>
2018-11-23 11:10:16 +0530

    Sample 2 added

--- src/testpack/TestCaseSample.java
+++ src/testpack/TestCaseSample.java
@@ -5,6 +5,8 @@ public class TestCaseSample {
 public static void main(String[] args) {
 	
 	System.out.println("Test Case Sample Added");
+	System.out.println("Test Case Sample Added 2");
+	
 } 	
 	
 }
