53b2100938903e01b963406e516f1e1f0f76bf7f
dkdeepakkumar3345 <dkdeepakkumar766@gmail.com>
2018-11-30 19:06:58 +0530

    checking conflict

--- src/testpack/TestCaseDeepak.java
+++ src/testpack/TestCaseDeepak.java
@@ -2,11 +2,22 @@ package testpack;
 
 public class TestCaseDeepak {
 	public void sampleInfo() {
-		System.out.println("Deepak Test Case");
+		  int r,sum=0,temp;    
+		  int n=454;
+		  
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;   
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println("palindrome number ");    
+		  else    
+		   System.out.println("not palindrome");
 	}
 
 	public static void main(String[] args) {
-		TestCaseDeepak deep = new TestCaseDeepak();
 		deep.sampleInfo();
 	}
 }
